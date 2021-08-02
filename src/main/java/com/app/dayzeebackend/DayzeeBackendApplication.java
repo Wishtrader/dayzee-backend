ee23f70fe195e1319bcf2711be501ad931b6ac43
Andrei Kamarou <mr.akomarov@gmail.com>
2021-08-02 03:23:04 +0300

    initial commit

---
+++ src/main/java/com/app/dayzeebackend/DayzeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.dayzeebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayzeeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DayzeeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/dayzeebackend/DayzeeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.dayzeebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayzeeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
